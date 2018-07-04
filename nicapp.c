e917b48d8ca8c7a2610f2dd44bb73732ced9e0d7
mrks <yuke1222@gmail.com>
2018-07-04 18:10:21 +0900

    ethtoolから抜き出した

---
+++ nicapp.c
@@ -0,0 +1,68 @@
+/*-
+ *   BSD LICENSE
+ *
+ *   Copyright(c) 2015 Intel Corporation. All rights reserved.
+ *   All rights reserved.
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+/*-
+ *   Original file: dpdk-stable-17.05.2/examples/ethtool/ethtool-app/ethapp.c
+ */
+
+#include "nicapp.h"
+
+void get_macaddr(uint8_t portid)
+{
+	struct ether_addr mac_addr;
+
+	stat = 0;
+	if (!rte_eth_dev_is_valid_port(port_id)) {
+		printf("Error: Invalid port number %i\n", port_id);
+		return;
+	}
+
+	rte_eth_macaddr_get(port_id, &mac_addr);
+	printf(
+		"Port %i MAC Address: %02x:%02x:%02x:%02x:%02x:%02x\n",
+		port_id,
+		mac_addr.addr_bytes[0],
+		mac_addr.addr_bytes[1],
+		mac_addr.addr_bytes[2],
+		mac_addr.addr_bytes[3],
+		mac_addr.addr_bytes[4],
+		mac_addr.addr_bytes[5]);
+}
+
+void nicapp_main(uint8_t cnt_ports)
+{
+	uint8_t i;
+
+	for (i=0; i<cnt_ports, i++) {
+		getmacaddr(i);
+	}
+}
